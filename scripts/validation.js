34cf323a2a36eb735763790be9b426cb9a911a30
nsithlopez <nsithlopez@gmail.com>
2023-08-09 12:48:39 -0400

    refactor: remove hardcode on button selector

--- scripts/validation.js
+++ scripts/validation.js
@@ -46,9 +46,9 @@ function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
 }
 
 function setEventListener(formEl, options) {
-  const { inputSelector } = options;
+  const { inputSelector, submitButtonSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
-  const submitButton = formEl.querySelector(".modal__button");
+  const submitButton = formEl.querySelector(submitButtonSelector);
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
